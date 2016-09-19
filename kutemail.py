26c1ebc13512022425398c7773a586897abf2765
Etenil <dev@etenil.net>
2016-09-19 15:21:18 +0100

    Added account dialog and settings.

--- kutemail.py
+++ kutemail.py
@@ -3,30 +3,34 @@ from time import sleep
 import threading
 from PyQt5 import QtWidgets
 from PyQt5 import uic
-
 from imaplib import IMAP4_SSL
+import pickle
+import os
+
+from pprint import pprint
+
+class Account():
+    config = {}
+    
+    def __init__(self):
+        self.config = pickle.load(open(os.path.expanduser("~/.config/kutemail.rc"), "rb"))
+    
+    def save(self):
+        pickle.dump(self.config, open(os.path.expanduser("~/.config/kutemail.rc"), "wb"))
 
 class MailRetriever(threading.Thread):
     imap_backend = None
-    onFinish = None
     
-    def __init__(self, onFinish):
+    def __init__(self):
         super(MailRetriever, self).__init__()
-        self.onFinish = onFinish
-        self.imap_backend = IMAP4_SSL(host="imap.gmail.com", 
+        self.imap_backend = IMAP4_SSL(host="imap.gmail.com")
+        self.imap_backend.login()
+        pprint(self.imap_backend.list())
+        print("\n")
     
-    def run(self):
+    def refresh(self, onFinish):
         sleep(3)
-        self.onFinish()
-
-#class ComposeDialog(QtWidgets.QDialog):
-    #def __init__(self):
-        #super(ComposeDialog, self).__init__()
-        #uic.loadUi('composedialog.ui', self)
-    
-    #def onSend(self):
-        #print('sending...')
-        #self.close()
+        onFinish()
 
 class ComposeWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -37,13 +41,27 @@ class ComposeWindow(QtWidgets.QMainWindow):
         print('sending...')
         self.close()
 
+class AccountDialog(QtWidgets.QDialog):
+    def __init__(self):
+        super(AccountDialog, self).__init__()
+        uic.loadUi('accountdialog.ui', self)
+    
+    def accept(self):
+        pass
+    
+    def reject(self):
+        pass
 
 class MainWindow(QtWidgets.QMainWindow):
     refreshing = False
+    mail_retriever = None
+    account = None
     
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('mainwindow.ui', self)
+        self.mail_retriever = MailRetriever()
+        self.account = Account()
 
     def advanceSlider(self):
         self.ui.progressBar.setValue(self.ui.progressBar.value() + 1)
@@ -57,13 +75,17 @@ class MainWindow(QtWidgets.QMainWindow):
         if not self.refreshing:
             self.refreshing = True
             self.statusbar.showMessage("Refreshing...")
-            retriever = MailRetriever(self.onRefreshEnd)
-            retriever.run()
+            self.mail_retriever.refresh(self.onRefreshEnd())
             
     def onRefreshEnd(self):
         self.refreshing = False
         self.statusbar.showMessage("")
-        
+    
+    def showEvent(self, event):
+        if self.acccount == None:
+            diag = AccountDialog()
+            diag.show()
+
 app = QtWidgets.QApplication(sys.argv)
 
 my_mainWindow = MainWindow()
